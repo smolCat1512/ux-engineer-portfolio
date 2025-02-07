713bec83503465854e03f9fc8c5ea62013b62d83
ShaunH1512 <shaunhalliday1512@gmail.com>
2025-02-07 15:58:11 +0000

    chore: Building Logo and Header
    Moved components folder as was erroring. Still working on this as a work in progress

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import "../app/globals.css";
-import Header from "./components/header";
+import Header from "@/components/header";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
