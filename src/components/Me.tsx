944f833d04953ba44c30a41f99614cc94ab370bb
ShaunH1512 <shaunhalliday1512@gmail.com>
2025-02-23 15:27:01 +0000

    chore: Hero section work
    Placeholder cat image - images seem to have to be pngs to use nextJs Image component.
    Popped in placeholder text for right hand section.

--- src/components/Me.tsx
+++ src/components/Me.tsx
@@ -1,6 +1,18 @@
+import Image from "next/image";
+import Cat from "../app/assets/cat1.png";
+
 const Me = () => {
-    return <section className="hi-screen flex-1 p-4">Me</section>;
-  };
-  
-  export default Me;
-  
+  return (
+    <section className="hi-screen flex-1 p-4">
+      <div className="flex flex-row justify-evenly">
+        <Image src={Cat} width={600} height={400} alt="holding image for hero section" priority/>
+        <div>
+        <h2 className="text-3xl font-bold">Shaun Halliday</h2>
+        <p className="text-purple-500">Hi this is text about me</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Me;
