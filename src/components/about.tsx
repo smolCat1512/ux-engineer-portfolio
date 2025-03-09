d3aa50bd135037271b791095920c7736beee4a56
ShaunH1512 <shaunhalliday1512@gmail.com>
2025-03-09 12:41:14 +0000

    chore: Scroll animation piece on About section

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,5 +1,19 @@
-const About = () => {
-  return <section className="hi-screen flex-1 p-4">About</section>;
-};
+// Mark this file as a Client Component
+"use client";
 
-export default About;
+import { motion } from "motion/react";
+
+export default function About() {
+  return (
+    <motion.section
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 4, ease: "easeOut" }}
+      viewport={{ once: true, amount: 0.4 }} // Ensures it only animates once when 10% of it is in view
+      className="hi-screen flex-1 p-4"
+      id="case-studies"
+    >
+      About
+    </motion.section>
+  );
+}
