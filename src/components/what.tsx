b410745a21697c75d8bc876bf43203655b10afab
ShaunH1512 <shaunhalliday1512@gmail.com>
2025-04-20 13:32:24 +0100

    chore: Resaerch component
    Going to use in how section, going to do each one like this, just a starter at this point to fill out content and not a final design and UI piece

--- src/components/what.tsx
+++ src/components/what.tsx
@@ -1,6 +1,7 @@
 // Mark this file as a Client Component
 "use client";
 
+import Research from "@/app/whatandhow/research";
 import { motion } from "framer-motion";
 
 const WhatAndHow = () => {
@@ -15,7 +16,7 @@ const WhatAndHow = () => {
     >
       <h2 className="text-3xl font-bold pb-6">What and How</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
-        <div className="flex justify-center">1</div>
+        <div className="flex justify-center align-middle"><Research /></div>
         <div className="flex justify-center">2</div>
         <div className="flex justify-center">3</div>
         <div className="flex justify-center">4</div>
